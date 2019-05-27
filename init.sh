e2ef4b538f76a9baf8beda4f86809af719c2023f
James Folk <james@njligames.com>
2019-05-27 19:06:35 -0400

    first commit

---
+++ init.sh
@@ -0,0 +1,13 @@
+mkdir Module1
+mkdir Module2
+mkdir Module3
+mkdir Module4
+mkdir Module5
+mkdir Module6
+mkdir Module7
+mkdir Module8
+mkdir Module9
+mkdir Module10
+mkdir Module11
+mkdir Module12
+
